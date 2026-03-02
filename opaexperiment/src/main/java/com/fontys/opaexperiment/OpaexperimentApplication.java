dfb49c62db46f9b73aff0440aa8298f0f238837b
Micahve <micah@familievaneverdingen.nl>
2026-03-02 11:29:22 +0100

    java spring boot setup

---
+++ opaexperiment/src/main/java/com/fontys/opaexperiment/OpaexperimentApplication.java
@@ -0,0 +1,13 @@
+package com.fontys.opaexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpaexperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpaexperimentApplication.class, args);
+    }
+
+}

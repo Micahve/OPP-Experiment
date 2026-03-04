b69fa4d721f38c5c915d088b63b029f01df74eda
Micahve <micah@familievaneverdingen.nl>
2026-03-04 11:15:08 +0100

    OPAConfiguration file

--- opaexperiment/src/main/java/com/fontys/opaexperiment/config/WebSecurityConfig.java
+++ opaexperiment/src/main/java/com/fontys/opaexperiment/config/WebSecurityConfig.java
@@ -1,19 +1,25 @@
 package com.fontys.opaexperiment.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import io.github.open_policy_agent.opa.springboot.OPAAuthorizationManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+    @Autowired
+    OPAAuthorizationManager opaAuthorizationManager;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()  // ← this line permits all
+        http.authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
                 )
-                .csrf(csrf -> csrf.disable()); // optional, disable CSRF for API testing
+                .csrf(csrf -> csrf.disable());
 
         return http.build();
     }

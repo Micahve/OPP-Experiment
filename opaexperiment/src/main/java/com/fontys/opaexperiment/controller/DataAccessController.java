a053bf16999868f5cff26b672ad8e857c1ab9aaf
Micahve <micah@familievaneverdingen.nl>
2026-03-06 15:14:06 +0100

    added example of getting role and username from the JWT token in the DataAccessController

--- opaexperiment/src/main/java/com/fontys/opaexperiment/controller/DataAccessController.java
+++ opaexperiment/src/main/java/com/fontys/opaexperiment/controller/DataAccessController.java
@@ -2,11 +2,20 @@ package com.fontys.opaexperiment.controller;
 import com.fontys.opaexperiment.data.DataStorage;
 import com.fontys.opaexperiment.entities.ResearchData;
 import com.fontys.opaexperiment.entities.User;
+import com.fontys.opaexperiment.service.JWTService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,9 +25,21 @@ public class DataAccessController {
     @Autowired
     private DataStorage dataStorage;
 
+    @Autowired
+    private JWTService jwtService;
+
     @GetMapping("/users")
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers(@RequestHeader("Authorization") String authHeader){
         System.out.println("Starting Get All Users Now!");
+
+        String token = authHeader.replace("Bearer ", "");
+
+        String username = jwtService.extractUsername(token);
+        String role = jwtService.extractRole(token);
+
+        System.out.println("Username: " + username);
+        System.out.println("Role: " + role);
+
         List<User> allUsers = dataStorage.getUsers();
         for (User user : allUsers){
             System.out.println(user.printUserInfo());
